3213842363ffe10bd819f0d4f4dc3565a5f366ff
Amy Sams <asams@mail.umw.edu>
2011-02-16 20:10:59 -0500

    trying to fix createaccount.php

--- createAccount.php
+++ createAccount.php
@@ -1,24 +1,39 @@
+<?php
+   session_start();
+?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>Log In</title>
   <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
+<?php
+  include "db_connect.php";
+?>
+
 
 <body>
 <div id="contents">
 <?php
-  include "db_connect.php";
   
   $username = $_POST['username'];
   $pw = $_POST['pw'];
   $zip = $_POST['zip'];
+ 
+  //echo $username;
+  //echo $pw;
+  //echo $zip;
   
-  $query = "select * from users WHERE username = '$username'";
-  $result = mysqli_query($db, $query);
-  if ($row != mysqli_fetch_array($result)) {
-    $query= "INSERT INTO users VALUES('$username', SHA($pw), '$zip') ";
-    $result = mysqli_query($db, $query) or die("Error Querying Database");
+  $query = "SELECT * from users WHERE username = '$username' and password=SHA('$pw')";
+  
+  //echo $query;
+  $result = mysqli_query($db, $query) or die("Error Querying Database1");
+  if ($row = mysqli_fetch_array($result)) {
+    echo 'That username already exists';
+  } else {
+     $query= "INSERT INTO users ('username', 'password', 'zipcode') VALUES('$username', SHA($pw), '$zip') ";
+     echo $query;
+     $result = mysqli_query($db, $query) or die("Error Querying Database2");
   }
 ?>  
 </div>
